1ab290e9f6e7c337f7307fde38f52917e3f9f44a
aaronshin43 <aaronshin.exe@gmail.com>
2026-01-30 00:34:30 -0500

    adjust lr_decay_iters to align with training iterations

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -26,7 +26,7 @@ dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
 max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+lr_decay_iters = 3000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
@@ -34,4 +34,4 @@ warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
 # device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+compile = False # do not torch compile the model
